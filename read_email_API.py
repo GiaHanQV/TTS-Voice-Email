6dcbddcdbca17268c71f8b1142d309d6b6949dab
GiaHanQV <huynhgia.han@quocviet.com.vn>
2026-03-13 23:22:04 +0700

    fix: remove Google API

--- read_email_API.py
+++ read_email_API.py
@@ -1,12 +1,11 @@
 import os
-import google.generativeai as genai
+# import google.generativeai as genai
 from streamlit import audio
 from vieneu import Vieneu
 
-
-GEMINI_API_KEY = ""
-genai.configure(api_key=GEMINI_API_KEY)
-llm_model = genai.GenerativeModel('models/gemini-2.0-flash')
+# GEMINI_API_KEY = "AIzaSyAv_2JMErdxLQCfOuYjxnIwc9zo0rVxvoM"
+# genai.configure(api_key=GEMINI_API_KEY)
+# llm_model = genai.GenerativeModel('models/gemini-2.0-flash')
 
 print("Đang tải mô hình VieNeu-TTS...")
 tts = Vieneu()
@@ -19,23 +18,19 @@ for desc, voice_id in available_voices:
     print(f"- {desc} (Mã ID: {voice_id})")
 giong_doc_truyen_cam = tts.get_preset_voice("Doan") 
 
-
-# ==========================================
-# 3. HÀM TÓM TẮT BẰNG AI
-# ==========================================
-def tom_tat_bang_ai(noi_dung_email):
-    prompt = f"""
-    Tóm tắt email sau thành 2 câu ngắn gọn tiếng Việt. 
-    Yêu cầu: Không dùng ký tự đặc biệt, văn phong thân thiện, tự nhiên để máy đọc cho người dùng nghe.
-    Nội dung: {noi_dung_email}
-    """
-    try:
-        response = llm_model.generate_content(prompt)
-        tom_tat = response.text.strip().replace("*", "")
-        return tom_tat
-    except Exception as e:
-        print(f"Lỗi API: {e}")
-        return noi_dung_email[:150] + " và một số nội dung khác."
+# def tom_tat_bang_ai(noi_dung_email):
+#     prompt = f"""
+#     Tóm tắt email sau thành 2 câu ngắn gọn tiếng Việt. 
+#     Yêu cầu: Không dùng ký tự đặc biệt, văn phong thân thiện, tự nhiên để máy đọc cho người dùng nghe.
+#     Nội dung: {noi_dung_email}
+#     """
+#     try:
+#         response = llm_model.generate_content(prompt)
+#         tom_tat = response.text.strip().replace("*", "")
+#         return tom_tat
+#     except Exception as e:
+#         print(f"Lỗi API: {e}")
+#         return noi_dung_email[:150] + " và một số nội dung khác."
 
 email_moi = {
     "sender": "Sếp Tổng <director@congty.com>",
@@ -44,22 +39,22 @@ email_moi = {
 }
 
 print("\n--- BẮT ĐẦU XỬ LÝ EMAIL ---")
-noi_dung_tom_tat = tom_tat_bang_ai(email_moi["body"])
-kich_ban_doc = f"Bạn có thư từ {email_moi['sender'].split('<')[0]}. Tiêu đề: {email_moi['subject']}. Nội dung tóm tắt: {noi_dung_tom_tat}"
+# noi_dung_tom_tat = tom_tat_bang_ai(email_moi["body"])
+# kich_ban_doc = f"Bạn có thư từ {email_moi['sender'].split('<')[0]}. Tiêu đề: {email_moi['subject']}. Nội dung tóm tắt: {noi_dung_tom_tat}"
+kich_ban_doc = f"Bạn có thư từ {email_moi['sender'].split('<')[0]}. Tiêu đề: {email_moi['subject']}. Nội dung: {email_moi['body']}"
 
 print(f"\nNội dung AI sẽ đọc: {kich_ban_doc}\n")
 print("Đang tạo file âm thanh...")
-
 
 #audio = tts.infer(text=kich_ban_doc, voice=giong_doc_truyen_cam)
 
 audio = tts.infer(
     text=kich_ban_doc,
-    ref_audio="VTV_Voice.wav", # Đường dẫn file audio 5s
+    ref_audio="VTV_Voice.wav", 
     ref_text="thưa quý vị, chỉ còn ba ngày nữa ngày mười lăm tháng ba năm hai nghìn không trăm hai mươi sáu cử tri cả nước nô nức đi bầu" 
 )
 
 
-output_path = "thong_bao_email/email_truyen_cam_GCS.wav"
+output_path = "thong_bao_email/email_truyen_cam_Full.wav"
 tts.save(audio, output_path)
 print(f"-> HOÀN TẤT! Đã lưu file tại: {output_path}")
